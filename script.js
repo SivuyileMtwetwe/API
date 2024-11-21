4df89ece92ff0d219fcd867b150fc1891b69e32b
SivuyileMtwetwe <sivuyilemtwetwe@gmail.com>
2024-11-21 15:43:28 +0200

    props

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const  fetchData = async () => {
     friends = data
 
     friends.forEach(item => {
-        console.log(item.firstName);
+        // console.log(item.firstName);
+        return `my name is ${item.firstName} my surname is ${item.lastName}, on my free time I like to enjoy my hobby which is ${item.favHobby}. My criminal status is ${item.criminal}`
     })
 
 }
